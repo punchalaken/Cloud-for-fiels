332cc05dd18b078642e3f628a27c527148b14b92
Semen Kliuev <penekkakti@gmail.com>
2025-02-15 00:37:48 +0300

    dell fronend

--- frontend/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-export {}; // Указываем, что файл является модулем
-
-declare global {
-    interface Window {
-        sharedUserId: number; // Добавляем свойство к глобальному объекту window
-    }
-}
